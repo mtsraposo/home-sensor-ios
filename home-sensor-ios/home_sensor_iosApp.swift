f13659eaaec26200a2576b21f79b249abcd434ef
Matheus Raposo <matheus_raposo@hotmail.com>
2023-10-07 09:41:05 -0300

    Initial Commit

---
+++ home-sensor-ios/home_sensor_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  home_sensor_iosApp.swift
+//  home-sensor-ios
+//
+//  Created by Matheus Raposo on 07/10/23.
+//
+
+import SwiftUI
+
+@main
+struct home_sensor_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
